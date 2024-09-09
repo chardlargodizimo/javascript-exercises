13d9c498364d77bd2bcb56ee5521139e4f29ab01
chardlargodizimo <chardlargodizimo.cl@gmail.com>
2024-09-09 14:17:36 +0800

    Test Driven Development (TDD) Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, runs) {
+  let str = "";
 
+  if (runs < 0) return "ERROR";
+
+  for (let i = 0; i < runs; i++) {
+    str += word;
+  }
+
+  return str;
 };
 
 // Do not edit below this line
