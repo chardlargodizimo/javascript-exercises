ee567d148662c4dac67a7653ae9d815eebc269ef
chardlargodizimo <chardlargodizimo.cl@gmail.com>
2024-09-09 23:05:27 +0800

    Test Driven Development (TDD) Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  // EASIER AND EASY TO UNDERSTAND SOLUTION
+
+  /* const newArray = [];
+  for (const element of array) {
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray; */
+
+  // SIMPLE AND ADVANCED SOLUTION
+
+  return array.filter((element) => !args.includes(element));
 };
 
 // Do not edit below this line
