e0b9269f4d479b3479fe5d96ed6826f8b90d253d
chardlargodizimo <chardlargodizimo.cl@gmail.com>
2024-09-17 22:43:48 +0800

    Test Driven Development (TDD) Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (m, n) {
+  let sum = 0;
 
+  if (!Number.isInteger(m) || !Number.isInteger(n) || m < 0 || n < 0)
+    return "ERROR";
+
+  if (m > n) [m, n] = [n, m];
+
+  for (let i = m; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
